8589347029bdba6e9339cce7d72a4fbcf67147ff
Khalitov62 <tu7m5rk73code.edu.az>
2022-06-21 15:01:53 +0400

    added

---
+++ API_Intro/Migrations/20220621105915_crDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_Intro.Migrations
+{
+    public partial class crDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
